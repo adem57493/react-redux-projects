a461a08dcfadfdd96f6e0102a4c336613904e6e4
adem57493 <150789425+adem57493@users.noreply.github.com>
2025-04-01 13:07:11 +0300

    Delete 30-onwards-to-a-biggest-projec-e directory

--- 30-onwards-to-a-biggest-projec-e/pages/_app.js
+++
@@ -1,9 +0,0 @@
-import "@/styles/globals.css";
-import Layout from '@/components/layout/Layout.js';
-/**bu root component sayılır */
-export default function App({ Component, pageProps }) {/**Component page contenti tutar dolayısıyla 
-  bir sayfayı değiştirdiğimizde farklı olacaktır bu proplar next.js tarafından otomatik olarak verilir */
-  return <Layout>
-    <Component {...pageProps} />
-     </Layout>
-}
